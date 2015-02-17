608d2704cd3a01c9209c8e9eac1fdcc6a479e0a4
Pete Turnbull <pt1812@imperial.ac.uk>
2015-02-17 05:23:51 +0000

    Fixed fabfiles

--- src/fabfile.py
+++ src/fabfile.py
@@ -1,12 +1,10 @@
-from __future__ import with_statement
-from fabric.api import *
-from fabric.contrib.console import confirm
+from fabric.api import run, env, cd
 
 env.user = 'pt1812'
 env.hosts = ['shell1.doc.ic.ac.uk']
 
 def run_worker():
     with cd('/homes/pt1812/Development/individual_project/src'):
-    	run('../libs/fab -f fabfile_internal.py _run_worker')
+    	run('../libs/fab -f fabfile_internal.py run_worker')
 
 
--- src/fabfile_internal.py
+++ src/fabfile_internal.py
@@ -1,10 +1,8 @@
-from __future__ import with_statement
-from fabric.api import *
-from fabric.contrib.console import confirm
+from fabric.api import run, env, cd
 
-env.hosts = ['pixel01, edge12']
+env.hosts = ['pixel01']
 
-def _run_worker():
+def run_worker():
     with cd('/homes/pt1812/Development/individual_project/src'):
     	run('nohup ../libs/celery worker -l info --concurrency=16 &')
 
