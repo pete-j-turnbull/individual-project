851ae842cbae500b3065506e501f42fc38da455f
Pete Turnbull <pt1812@imperial.ac.uk>
2015-02-17 11:07:44 +0000

    Added dtach exec

--- src/fabfile.py
+++ src/fabfile.py
@@ -6,5 +6,4 @@ env.gateway = 'shell1.doc.ic.ac.uk'
 
 def run_worker():
     with cd('/homes/pt1812/Development/individual_project/src'):
-    	run('nohup ../libs/celery worker -l info --concurrency=16 &')
-
+    	run("dtach ../libs/celery worker -l info --concurrency=16")
