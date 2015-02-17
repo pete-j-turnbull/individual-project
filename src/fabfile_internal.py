8886f682acfa2b153cd9b0b5dbe7f0ac523f1e8a
Pete Turnbull <pt1812@imperial.ac.uk>
2015-02-17 05:08:16 +0000

    Core change in fabfile

--- src/fabfile_internal.py
+++ src/fabfile_internal.py
@@ -6,5 +6,5 @@ env.hosts = ['pixel01, edge12']
 
 def _run_worker():
     with cd('/homes/pt1812/Development/individual_project/src'):
-    	run('nohup ../libs/celery worker -l info --concurrency=8 &')
+    	run('nohup ../libs/celery worker -l info --concurrency=16 &')
 
