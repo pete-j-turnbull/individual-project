da40f4e961945c7d9ab2976660b0673ddfff86d1
Pete Turnbull <pt1812@imperial.ac.uk>
2015-05-20 17:59:03 +0100

    Fixed shit

--- src/ebay_loader.py
+++ src/ebay_loader.py
@@ -9,12 +9,28 @@ MONGO_PORT = '27017'
 connection = pymongo.Connection('mongodb://%s:%s' % (MONGO_IP, MONGO_PORT), safe=True)
 
 #Randomly load from the db n data points
-def load_data(categories, bid_section, n):
+def load_data(category, bid_section, n):
 	db = connection['auction_items']
-	items = db._171957.find()
+	items = None
+
+	if category == 9355:
+		items = db._9355.find()
+	elif category == 175672:
+		items = db._175672.find()
+	elif category == 171957:
+		items = db._171957.find()
+	elif category == 171485:
+		items = db._171485.find()
+	elif category == 15052:
+		items = db._15052.find()
+	elif category == 32852:
+		items = db._32852.find()
+	elif category == 50582:
+		items = db._50582.find()
+
 	dataset = []
 
-	for i in range(25, n):
+	for i in range(0, n):
 		try:
 			code.interact(local=locals())
 			center = parse_center(items[i]['html1']['CenterPanelInternal'])
