8d9da970ffd59d7f636f509dcd2007f24ea4322d
Pete Turnbull <pt1812@imperial.ac.uk>
2015-05-20 17:25:48 +0100

    d

--- src/ebay_loader.py
+++ src/ebay_loader.py
@@ -1,5 +1,6 @@
 import pymongo
 from bson import ObjectId
+from tasks import *
 import code
 
 MONGO_IP = '146.169.47.50'
@@ -8,11 +9,18 @@ MONGO_PORT = '27017'
 connection = pymongo.Connection('mongodb://%s:%s' % (MONGO_IP, MONGO_PORT), safe=True)
 
 #Randomly load from the db n data points
-def load(categories, bid_section, n):
+def load_data(categories, bid_section, n):
 	db = connection['auction_items']
 	items = db._171957.find()
-	loaded = []
+	dataset = []
 
-	for i in range(0, items.count()):
-		loaded.append(items[i]['item_id'])
-	return loaded
+	for i in range(0, n):
+		center = parse_center(items[i]['html1']['CenterPanelInternal'])
+		vals = center.values()
+		_d = {'item_title': vals[0], 'seller_rating': vals[1], 'seller_percentage': vals[4], 'num_images': len(vals[5]), 'end_timestamp': vals[6], 'condition': vals[7]}
+		dataset.append(_d)
+	return dataset
+
+dataset = load_data(None, None, 100)
+
+code.interact(local=locals())
