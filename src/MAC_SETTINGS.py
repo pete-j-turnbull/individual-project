fedd5d746dd786bb3055d4ae9900b3e90f300f02
Pete Turnbull <pt1812@imperial.ac.uk>
2015-06-21 02:58:09 +0100

    .

---
+++ src/MAC_SETTINGS.py
@@ -0,0 +1,14 @@
+DEBUG = False
+
+MONGO_IP = '146.169.47.50'
+MONGO_PORT = '27017'
+BLOOM_DIR = '/Users/pete/Development/individual_project/storage/bloomfilters'
+
+LOG_DIR = '/Users/pete/Development/individual_project/storage/logs'
+
+
+# CELERY and rabbitmq BROKER settings
+BROKER_URL = "amqp://guest:guest@146.169.47.6:5672//"
+#BROKER_URL = "amqp://guest:guest@146.169.46.139:5672//"
+CELERY_RESULT_BACKEND = "amqp"
+CELERY_IMPORTS=("tasks",)
