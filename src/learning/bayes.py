d10917849957ef3e50f8236cf0c99d8542f2e5d7
Pete Turnbull <pt1812@imperial.ac.uk>
2015-06-21 02:30:01 +0100

    .

--- src/learning/bayes.py
+++ src/learning/bayes.py
@@ -1,6 +1,63 @@
 from ebay_loader import load_data
 
-dataset = load_data(9355, False, 200)
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
 
-for d in dataset:
-	print '%s : %s' % (d['item_title'], d['price'])
+(training_set, testing_set) = load_data(9355, 6, 6)
+
+CAT_SPACING = 20
+
+
+titles = []
+price_cats = []
+for d in training_set:
+	titles.append(d[0]['item_title'])
+	price_cat = (d[1] - (d[1] % CAT_SPACING)) / CAT_SPACING
+	price_cats.append(price_cat)
+
+
+vectorizer = CountVectorizer(min_df=1)
+
+
+_X = vectorizer.fit_transform(titles)
+
+tf_transformer = TfidfTransformer()
+X = tf_transformer.fit_transform(_X)
+
+model = MultinomialNB().fit(X, price_cats)
+
+
+#Now run the test data through the model
+titles = []
+for d in testing_set:
+	titles.append(d[0]['item_title'])
+
+X = tf_transformer.transform( vectorizer.transform(titles) )
+prices_pred = map((lambda x: (x * CAT_SPACING) + CAT_SPACING / 2), model.predict(X))
+
+for (title, price) in zip(titles, prices_pred):
+	print '%s => $%s' % (title, price)
+
+
+
+#Calc percentage error.
+def _compare(test_set, predictions):
+	error = 0
+	total = 0
+	for (t1, t2) in zip(test_set, predictions):
+		diff = t1[1] - t2
+		error += diff*diff
+		total += t1[1]
+	p = error / total
+	return p
+
+
+
+
+
+#Compare predictions against actual and print out the results
+results = _compare(testing_set, prices_pred)
+print results
+
+
--- src/learning/ebay_loader.py
+++ src/learning/ebay_loader.py
@@ -8,37 +8,55 @@ from tasks import *
 MONGO_IP = '146.169.47.50'
 MONGO_PORT = '27017'
 
-connection = pymongo.Connection('mongodb://%s:%s' % (MONGO_IP, MONGO_PORT), safe=True)
+#connection = pymongo.Connection('mongodb://%s:%s' % (MONGO_IP, MONGO_PORT), safe=True)
+
+data = [({'item_title': 'Apple iPhone 5 - 32GB - Black &amp; Slate (AT&amp;T) Smartphone'}, 207.50), ({'item_title': 'Apple iPhone 6 (Latest Model) - 16GB - Silver (AT&amp;T) Smartphone'}, 590.0), ({'item_title': 'Apple iPhone 5 - 16GB - Black &amp; Slate (AT&amp;T) Smartphone'}, 67.0), ({'item_title': 'Apple iPhone 3G (8 GB) UNLOCKED'}, 39.99), 
+		({'item_title': 'Apple iPhone 5c - 32GB - Pink (AT&amp;T) Smartphone'}, 242.50), ({'item_title': 'Samsung Galaxy S5 Active SM-G870A (Latest Model) - 16GB - Camo Green (AT&amp;T)...'}, 455.0), ({'item_title': 'Apple iPhone 5s - 16GB - Space Gray (Verizon) Smartphone'}, 255.0), ({'item_title': 'Apple iPhone 4 - 8GB - Black (AT&amp;T) Smartphone (MD126LL/A)'}, 49.0), 
+		({'item_title': 'Apple iPhone 5 - 32GB - White &amp; Silver (AT&amp;T) Smartphone'}, 192.50), ({'item_title': 'Apple iPhone 5 - 16GB - Black &amp; Slate (Verizon) Smartphone'}, 200.0), ({'item_title': 'Apple iPhone 5c - 16GB - Blue (T-Mobile) Smartphone'}, 173.50), ({'item_title': 'Apple iPhone 4S (16GB) Black VERIZON Smartphone C'}, 79.91)]
+
 
 #Randomly load from the db n data points
-def load_data(category, bid_section, n):
-	db = connection['auction_items']
-	items = None
+def load_data(category, n_train, n_test):
+	#db = connection['auction_items']
+	#items = None
 
-	if category == 9355:
-		items = db._9355.find()
-	elif category == 175672:
-		items = db._175672.find()
-	elif category == 171957:
-		items = db._171957.find()
-	elif category == 171485:
-		items = db._171485.find()
-	elif category == 15052:
-		items = db._15052.find()
-	elif category == 32852:
-		items = db._32852.find()
-	elif category == 50582:
-		items = db._50582.find()
+	#if category == 9355:
+	#	items = db._9355.find()
+	#elif category == 175672:
+	#	items = db._175672.find()
+	#elif category == 171957:
+	#	items = db._171957.find()
+	#elif category == 171485:
+	#	items = db._171485.find()
+	#elif category == 15052:
+	#	items = db._15052.find()
+	#elif category == 32852:
+	#	items = db._32852.find()
+	#elif category == 50582:
+	#	items = db._50582.find()
 
-	dataset = []
+	training_set = []
+	testing_set = []
 
-	for i in range(0, n):
+	for i in range(0, n_train):
 		try:
-			center = parse_center(items[i]['html1']['CenterPanelInternal'])
-			vals = center.values()
-			_d = {'item_title': vals[0], 'price': vals[1], 'seller_rating': vals[2], 'seller_percentage': vals[5], 'num_images': len(vals[6]), 'end_timestamp': vals[7], 'condition': vals[8]}
-			dataset.append(_d)
+			#center = parse_center(items[i]['html1']['CenterPanelInternal'])
+			#vals = center.values()
+			#d1 = {'item_title': vals[0], 'seller_rating': vals[2], 'seller_percentage': vals[5], 'num_images': len(vals[6]), 'end_timestamp': vals[7], 'condition': vals[8]}
+			#d2 = vals[1]
+			training_set.append(data[i])
 		except Exception as e:
 			print i
-	return dataset
 
+	for i in range(n_train, n_train + n_test):
+		try:
+			#center = parse_center(items[i]['html1']['CenterPanelInternal'])
+			#vals = center.values()
+			#d1 = {'item_title': vals[0], 'seller_rating': vals[2], 'seller_percentage': vals[5], 'num_images': len(vals[6]), 'end_timestamp': vals[7], 'condition': vals[8]}
+			#d2 = vals[1]
+			testing_set.append(data[i])
+		except Exception as e:
+			print i
+
+
+	return (training_set, testing_set)
